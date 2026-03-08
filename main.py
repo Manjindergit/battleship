6cfbd44098e59c2b2cc8074354009938b525c5c7
Manjinderjit <manjindergrewal1997@gmail.com>
2026-03-07 16:11:56 -0800

    draw player

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
 from logger import log_state
 from constants import *
+import player
+
 
 def main():
     print("Hello from battleship!")
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     
     running = True
+    dt = 0
+    p1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     
     while running:
         
@@ -18,9 +25,11 @@ def main():
                 running = False
                 
         screen.fill("yellow")
+        p1.draw(screen)
         
         pygame.display.flip()
-        clock.tick(60)  # limits FPS to 60
+        dt = clock.tick(60)/1000 # delay game to 1/60 of second to prevent resource consumption and control frame rate
+        
 
 if __name__ == "__main__":
     main()
